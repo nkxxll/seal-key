bd41522310244845688617737c037c51ee4dd9e1
nkxxll <niki.nice1203@gmail.com>
2024-03-15 19:36:51 +0100

    feat: read key from file or stdin or cli
    added debug macros
    added read key from file
    added read key from stdin
    added read key from cli
    added optional key size this is 32 by default need to think about that
      need to think about aes encryption also
    this is still in development mode:
      we need get key option
      we need a name so we can store multiple keys
      we need some tests in some way defined this is too unstable

--- host/main.c
+++ host/main.c
@@ -26,7 +26,9 @@
  */
 
 #include <err.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /* OP-TEE TEE client API (built by optee_client) */
@@ -35,11 +37,114 @@
 /* TA API: UUID and command IDs */
 #include <seal-key_ta.h>
 
+#define DEFAULT_KEY_SIZE 32
+#define INFO(fmt, ...)                                                         \
+  printf("[+] %s:%d " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
+#define WARN(fmt, ...)                                                         \
+  printf("[!] %s:%d " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
+#define DEBG(fmt, ...)                                                         \
+  printf("[*] %s:%d " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
+#define ERRO(fmt, ...)                                                         \
+  printf("[[-!-]] %s:%d " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
+
 /* TEE resources */
 struct test_ctx {
   TEEC_Context ctx;
   TEEC_Session sess;
 };
+
+/**
+ * @brief usage of the app
+ *
+ * this function is called when the app is called with wrong parameters
+ * it shows how to use the app in the console using Linux style
+ */
+void usage(const char *prog_name) {
+  printf("Usage: %s [OPTION]... [KEY|KEY_FILE] [SIZE]\n", prog_name);
+  printf("Concatenate KEY(s) to standard output.\n");
+  printf("If no KEY is given, or if KEY is '-', read standard input.\n\n");
+  printf("Flags:\n");
+  printf("\t-h, --help\tDisplay this help message and exit\n");
+  printf("\t-f, --file <file>\tUse the key from the file\n");
+  printf("\t-k, --key <key>\tUse the key from the command line directly\n");
+  printf("SIZE\tSize of key in bytes (optional)\n");
+}
+
+void parse_args(int argc, char *argv[], int *size, int *file, int *key) {
+  // max 3 args name, option, key/file
+  if (argc > 4) {
+    usage(argv);
+    exit(1);
+  }
+  if (argc == 2 &&
+      (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)) {
+    usage(argv);
+    exit(0);
+  } else if (argc == 2 &&
+             (strcmp(argv[1], "--key") == 0 || strcmp(argv[1], "-k") == 0)) {
+    *key = 1;
+    return;
+  }
+  if (argc >= 3) {
+    if (strcmp(argv[1], "--file") == 0 || strcmp(argv[1], "-f") == 0) {
+      *file = 1;
+      if (argc == 4) {
+        *size = atoi(argv[3]);
+      }
+      return;
+    } else if (strcmp(argv[1], "--key") == 0 || strcmp(argv[1], "-k") == 0) {
+      *key = 1;
+      if (argc == 4) {
+        *size = atoi(argv[3]);
+      }
+      return;
+    } else if (strcmp(argv[1], "--size") == 0 || strcmp(argv[1], "-s") == 0) {
+      *size = 1;
+      if (argc == 4) {
+        *size = atoi(argv[3]);
+      }
+      return;
+    }
+  }
+  usage(argv[0]);
+  exit(1);
+}
+
+void read_key_file(char *file, char *key, size_t key_len) {
+  FILE *f;
+  f = fopen(file, "r");
+  if (f == NULL) {
+    printf("Error opening file %s\n", file);
+    exit(1);
+  }
+  char *res = fgets(key, key_len, f);
+  if (res == NULL) {
+    printf("Error reading file %s\n", file);
+    exit(1);
+  }
+  int fcres = fclose(f);
+  if (fcres != 0) {
+    printf("Error closing file %s\n", file);
+    exit(1);
+  }
+}
+
+void read_key(int argc, char *argv[], char *key, size_t key_len) {
+  if (argc == 2) {
+    printf("Enter the key: ");
+    char *res = fgets(key, key_len, stdin);
+    if (res == NULL) {
+      puts("Error reading file stdin\n");
+      exit(1);
+    }
+  }
+  // else it is parsed via the command line in the 3 position
+  char *res = strncpy(key, argv[2], key_len);
+  if (res == NULL) {
+    puts("Error copying file contents\n");
+    exit(1);
+  }
+}
 
 void prepare_tee_session(struct test_ctx *ctx) {
   TEEC_UUID uuid = TA_SEAL_KEY_UUID;
@@ -63,6 +168,8 @@ void terminate_tee_session(struct test_ctx *ctx) {
   TEEC_FinalizeContext(&ctx->ctx);
 }
 
+// todo: this is a function we can use our object here is the key in this case
+// of a seal key application
 TEEC_Result read_secure_object(struct test_ctx *ctx, char *id, char *data,
                                size_t data_len) {
   TEEC_Operation op;
@@ -93,6 +200,10 @@ TEEC_Result read_secure_object(struct test_ctx *ctx, char *id, char *data,
   return res;
 }
 
+// here we write the key to the optee secure storage
+// it gets sealed from the optee and stored securely
+// todo: ?do we trust this or do we want to encrypt it before sending it to the
+// optee?
 TEEC_Result write_secure_object(struct test_ctx *ctx, char *id, char *data,
                                 size_t data_len) {
   TEEC_Operation op;
@@ -150,73 +261,62 @@ TEEC_Result delete_secure_object(struct test_ctx *ctx, char *id) {
   return res;
 }
 
-#define TEST_OBJECT_SIZE 7000
-
-int main(void) {
+int main(int argc, char *argv[]) {
+  size_t key_len = DEFAULT_KEY_SIZE;
   struct test_ctx ctx;
-  char obj1_id[] = "object#1"; /* string identification for the object */
-  char obj2_id[] = "object#2"; /* string identification for the object */
-  char obj1_data[TEST_OBJECT_SIZE];
-  char read_data[TEST_OBJECT_SIZE];
+  // todo: do we want to be able to add an identifier to the key?
+  int bool_key = 0;
+  int bool_file = 0;
+  char key[] = "key#1"; /* string identification for the object */
+
+  parse_args(argc, argv, &key_len, &bool_file, &bool_key);
+
+  char key_data[key_len];
+  char read_data[key_len];
   TEEC_Result res;
 
-  printf("Prepare session with the TA\n");
+  DEBG("key: %d, file %d", bool_key, bool_file);
+
+  // test this after
+  if (bool_key == 1) {
+    read_key(argc, argv, key_data, sizeof(key_data));
+    DEBG("%s", key_data);
+  } else if (bool_file == 1) {
+    read_key_file(argv[2], key_data, sizeof(key_data));
+    DEBG("%s", key_data);
+  } else {
+    // that is a safety net haha ;D
+    usage(argv);
+    exit(1);
+  }
+
+  INFO("Prepare session with the TA\n");
   prepare_tee_session(&ctx);
 
-  /*
-   * Create object, read it, delete it.
-   */
-  printf("\nTest on object \"%s\"\n", obj1_id);
+  INFO("- Create and load key in the TA secure storage\n");
 
-  printf("- Create and load object in the TA secure storage\n");
+  // INFO("this still only 0xA1 here update that\n");
+  // memset(key_data, 0xA1, sizeof(key_data));
 
-  memset(obj1_data, 0xA1, sizeof(obj1_data));
-
-  res = write_secure_object(&ctx, obj1_id, obj1_data, sizeof(obj1_data));
+  res = write_secure_object(&ctx, key, key_data, sizeof(key_data));
   if (res != TEEC_SUCCESS)
     errx(1, "Failed to create an object in the secure storage");
 
-  printf("- Read back the object\n");
+  INFO("- Read back the object\n");
 
-  res = read_secure_object(&ctx, obj1_id, read_data, sizeof(read_data));
+  res = read_secure_object(&ctx, key, read_data, sizeof(read_data));
   if (res != TEEC_SUCCESS)
     errx(1, "Failed to read an object from the secure storage");
-  if (memcmp(obj1_data, read_data, sizeof(obj1_data)))
+  if (memcmp(key_data, read_data, sizeof(key_data)))
     errx(1, "Unexpected content found in secure storage");
 
-  printf("- Delete the object\n");
-
-  res = delete_secure_object(&ctx, obj1_id);
+  // still for testing i think
+  INFO("- Delete the key after reading it\n");
+  res = delete_secure_object(&ctx, key);
   if (res != TEEC_SUCCESS)
     errx(1, "Failed to delete the object: 0x%x", res);
 
-  /*
-   * Non volatile storage: create object2 if not found, delete it if found
-   */
-  printf("\nTest on object \"%s\"\n", obj2_id);
-
-  res = read_secure_object(&ctx, obj2_id, read_data, sizeof(read_data));
-  if (res != TEEC_SUCCESS && res != TEEC_ERROR_ITEM_NOT_FOUND)
-    errx(1, "Unexpected status when reading an object : 0x%x", res);
-
-  if (res == TEEC_ERROR_ITEM_NOT_FOUND) {
-    char data[] = "This is data stored in the secure storage.\n";
-
-    printf("- Object not found in TA secure storage, create it.\n");
-
-    res = write_secure_object(&ctx, obj2_id, data, sizeof(data));
-    if (res != TEEC_SUCCESS)
-      errx(1, "Failed to create/load an object");
-
-  } else if (res == TEEC_SUCCESS) {
-    printf("- Object found in TA secure storage, delete it.\n");
-
-    res = delete_secure_object(&ctx, obj2_id);
-    if (res != TEEC_SUCCESS)
-      errx(1, "Failed to delete an object");
-  }
-
-  printf("\nWe're done, close and release TEE resources\n");
+  INFO("\nWe're done, close and release TEE resources\n");
   terminate_tee_session(&ctx);
   return 0;
 }
